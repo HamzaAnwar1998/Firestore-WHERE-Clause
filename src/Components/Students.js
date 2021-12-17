20ac8a68a3dae6a2992e91a4c1a7672852010e10
HamzaAnwar1998 <anwarhamza919@gmail.com>
2021-12-17 11:04:47 +0500

    key prop added

--- src/Components/Students.js
+++ src/Components/Students.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Students = ({students}) => {
     return students.map((individualStudent)=>(
-        <div className='student'>
+        <div className='student' key={individualStudent.ID}>
             <h2>Name: {individualStudent.Name}</h2>
             <h5>Class: {individualStudent.Class}</h5>
             <h5>GPA: {individualStudent.GPA}</h5>
